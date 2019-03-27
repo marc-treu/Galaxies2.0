4bf06cfb001b1ce2cb8a8f6708b62e30b710a3c4
marc <mr.treu@gmail.com>
2019-03-27 18:50:38 +0100

    Creation de la fonction qui permet de demander a l'utilisateur une requete

--- Galaxies/InterfaceGalaxies.py
+++ Galaxies/InterfaceGalaxies.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import ttk
-
+import baseDonnees
 
 import baseDonnees
 
@@ -83,7 +83,7 @@ class InterfaceGalaxies(tk.Tk):
         tk.Label(processing, text="Preprocessing").grid(row=0, column=0,sticky=tk.N+tk.S+tk.W+tk.E)#.pack(side=tk.LEFT,fill="both", expand = True)
         ttk.Separator(processing, orient=tk.VERTICAL).grid(row=0,rowspan = 4, column=1,sticky=tk.N+tk.S)#.pack(side=tk.LEFT, fill="y",padx=3, pady=1)#, expand = True)
         tk.Label(processing, text="Postprocessing").grid(row=0, column=2,sticky=tk.N+tk.S+tk.W+tk.E)#.pack(side=tk.RIGHT,fill="both", expand = True)
-        tk.Button(processing, text="Nouvelle Requete").grid(row=1,rowspan=3, column=0)
+        tk.Button(processing, text="Nouvelle Requete",command = self.new_requete).grid(row=1,rowspan=3, column=0)
         tk.Button(processing, text="Appliquer un filtre\n sur les noeuds").grid(row=1,rowspan=3, column=2)
 
         button = tk.Frame(self.frame_right_button)
@@ -98,6 +98,100 @@ class InterfaceGalaxies(tk.Tk):
 
     def action2(self):
         messagebox.showinfo("alerte", "Bravo!")
+
+    def new_requete(self):
+
+        def recupere():
+            if (auteur.get()):
+                requete['auteur'] = auteur.get().split()
+            if (notauteur.get()):
+                requete['-auteur'] = notauteur.get().split()
+            if (mcles.get()):
+                requete['mots_titre'] = mcles.get().split()
+            if (notmcles.get()):
+                requete['-mots_titre'] = notmcles.get().split()
+            if (date.get()):
+                requete['date'] = [int(s) for s in date.get().split() if s.isdigit()]
+                if (len(requete['date']) == 0):
+                    del requete['date']
+                elif (len(requete['date']) == 1):
+                    i = 1
+                    while (date.get()[i] == ' '):
+                        i = i + 1
+                    if (date.get()[i] == '-'):
+                        requete['date'].insert(0, "-")
+                    else:
+                        requete['date'].append("-")
+                elif (len(requete['date']) == 0):
+                    print("Erreur à la déclaration de l'intervalle")
+                elif requete['date'][0] > requete['date'][1]:
+                    print(
+                        "Erreur dans la déclaration de l'intervalle\nDonner en premier argument une date antérieure à la seconde")
+            if (empan.get()):
+                requete['empan'] = [int(s) for s in empan.get().split() if s.isdigit()].pop()
+            if (ltexte_max.get()):
+                requete['longueur_texte_maximal'] = [int(s) for s in ltexte_max.get().split() if s.isdigit()].pop(0)
+            if (nbnoeuds_min.get()):
+                requete['nbre_minimal_noeuds'] = [int(s) for s in nbnoeuds_min.get().split() if s.isdigit()].pop(0)
+            fenetre.destroy()
+
+        fenetre = tk.Tk()
+        fenetre.title("Recherche dans les galaxies")
+        fenetre.geometry("800x350")
+
+        instructions = tk.Label(fenetre, text="\nEntrer les critères de recherche\n", font="FreeSerif 14 bold").grid(row=0,
+                                                                                                                  column=1)
+
+        lab = tk.Label(fenetre, text="Noms et prénoms d'auteurs présents dans la galaxie :", font="Arial 11").grid(
+            sticky="w", row=1, column=1)
+        auteur = tk.Entry(fenetre, width=100)
+        auteur.grid(row=1, column=2)
+
+        lab2 = tk.Label(fenetre, text="Noms et prénoms d'auteurs absents de la galaxie :", font="Arial 11").grid(
+            sticky="w", row=2, column=1)
+        notauteur = tk.Entry(fenetre, width=100)
+        notauteur.grid(row=2, column=2)
+
+        lab3 = tk.Label(fenetre, text="Date de publication :\n(Donner un intervalle)", font="Arial 11").grid(sticky="w",
+                                                                                                          row=3,
+                                                                                                          column=1)
+        date = tk.Entry(fenetre, width=80)
+        date.insert(0, '[ AAAA - AAAA ]')
+        date.grid(sticky="w", row=3, column=2)
+
+        lab4 = tk.Label(fenetre, text="Mots présents dans le titre :", font="Arial 11").grid(sticky="w", row=4, column=1)
+        mcles = tk.Entry(fenetre, width=100)
+        mcles.grid(row=4, column=2)
+
+        lab5 = tk.Label(fenetre, text="Mots absents des titres :", font="Arial 11").grid(sticky="w", row=5, column=1)
+        notmcles = tk.Entry(fenetre, width=100)
+        notmcles.grid(row=5, column=2)
+
+        lab6 = tk.Label(fenetre, text="Longueur minimale du texte :", font="Arial 11").grid(sticky="w", row=6, column=1)
+        empan = tk.Entry(fenetre, width=30)
+        empan.grid(sticky="w", row=6, column=2)
+
+        lab7 = tk.Label(fenetre, text="Longueur minimale du plus long texte de la galaxie :", font="Arial 11").grid(
+            sticky="w", row=7, column=1)
+        ltexte_max = tk.Entry(fenetre, width=30)
+        ltexte_max.grid(sticky="w", row=7, column=2)
+
+        lab8 = tk.Label(fenetre, text="Nombre minimal de noeuds dans la galaxie :", font="Arial 11").grid(sticky="w",
+                                                                                                       row=8, column=1)
+        nbnoeuds_min = tk.Entry(fenetre, width=30)
+        nbnoeuds_min.grid(sticky="w", row=8, column=2)
+
+        requete = dict()
+
+        space = tk.Label(fenetre, text="\n").grid(row=10)
+        bouton = tk.Button(fenetre, text="Valider", command=recupere).grid(row=11, column=2)
+        close = tk.Button(fenetre, text="Fermer", command=fenetre.quit).grid(sticky="w", row=11, column=1)
+        fenetre.bind("<Return>", lambda e: recupere())
+        fenetre.bind("<Escape>", lambda e: fenetre.quit())
+
+        fenetre.mainloop()
+        print(requete)
+        return requete
 
     def display_graphe_list(self,listGraphe):
         """
@@ -136,8 +230,8 @@ class InterfaceGalaxies(tk.Tk):
 if __name__ == '__main__':
     interface = InterfaceGalaxies()
 
-    #listGraph = baseDonnees.getListeGraphe()
-    listGraph = ["Graphe "+str(i)+" de 4 noeuds" for i in range(100)]
+    listGraph = baseDonnees.getListeGraphe()
+    #listGraph = ["Graphe "+str(i)+" de 4 noeuds" for i in range(100)]
     interface.display_graphe_list(listGraph)
     interface.mainloop()
     #interface.destroy()
