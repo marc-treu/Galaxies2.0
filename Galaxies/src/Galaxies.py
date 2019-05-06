8474f978d6dac76104f9cbca1992dbc183495318
marc-treu <37772463+marc-treu@users.noreply.github.com>
2019-05-06 19:36:49 +0200

    Suppression d’une ligne devenue obsolète

--- Galaxies/src/Galaxies.py
+++ Galaxies/src/Galaxies.py
@@ -66,8 +66,7 @@ class Galaxie:
         self.interface.set_progress_bar_values(50, 100)
         t2 = time.clock()
         self.print_verbose("Temps de lecture du fichier source: " + format(t2 - t1, 'f') + " sec.")
-        self.print_verbose("premier line de la BD = ", grapheGalaxies.grapheConstruit(self.project_path))
-
+        
         maxNoeud = grapheGalaxies.construction_graphe(self.project_path)
         self.interface.set_progress_bar_values(60, 100)
         grapheGalaxies.sauvegarde_graphe_(self.project_path)  # Et on le sauvegarde
