9f4668aa4a859e2a1978c8e9c4dea2cc736638af
marc <mr.treu@gmail.com>
2019-05-28 14:46:56 +0200

    premier teste avec tsne

--- Galaxies/src/visualisation.py
+++ Galaxies/src/visualisation.py
@@ -4,6 +4,7 @@ import shelve
 
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+from yellowbrick.text import TSNEVisualizer
 
 
 def get_books(project_path):
@@ -25,6 +26,9 @@ def get_id_nodes_book(project_path, id_book):
 
 
 def get_galaxie_from_id_node(project_path, id_node):
+
+    # listeNodes n'existe plus
+
     dirNode = shelve.open(project_path + '/BDs/listeNodes')
     id_galaxie = dirNode[str(id_node)]
     dirNode.close()
@@ -39,8 +43,10 @@ def get_list_galaxie(project_path):
     connexion.close()
     return [i[0] for i in result]
 
+
 def inverse_bd(project_path, list_galaxie):
     dirGalaxies = shelve.open(project_path + '/BDs/listeGalaxies')
+    # listeNodes n'existe plus
     dirNode = shelve.open(project_path + '/BDs/listeNodes')
 
     for id_galaxie in list_galaxie:
@@ -50,14 +56,52 @@ def inverse_bd(project_path, list_galaxie):
     dirGalaxies.close()
     dirNode.close()
 
+
 if __name__ == '__main__':
-    path = "/home/marc/Desktop/PLDAC/Galaxies/projects/Balzac"
+    path = "../projects/Balzac"
 
     # liste_galaxie = get_list_galaxie(path)
     # print([liste_galaxie])
     # inverse_bd(path,liste_galaxie)
 
     t = get_books(path)
+    result = []
+    for i in range(1, len(t) + 1):
+        result.append(get_id_nodes_book(path, i))
+
+    index = dict()
+    for i in range(len(result)):
+        for j in range(len(result[i])):
+            index[result[i][j]] = i
+
+    liste_galaxies = get_list_galaxie(path)
+
+    matrix = np.zeros([len(t), len(liste_galaxies)])
+
+    print("index =", index)
+    print("matrix =", matrix)
+    print("result =", result)
+    print("liste_galaxies =", liste_galaxies)
+
+    dirGalaxies = shelve.open(path + '/BDs/listeGalaxies')
+
+    for galaxie in range(len(liste_galaxies)):
+        for node in dirGalaxies[str(liste_galaxies[galaxie])]:
+            matrix[index[node]][galaxie] = 1
+
+    dirGalaxies.close()
+    print("matrix =", matrix)
+
+    label = np.array([i for i in range(len(t))])
+
+    tsne = TSNEVisualizer(decompose_by=20)
+    tsne.fit(matrix, label)
+    tsne.poof()
+
+
+
+
+
     # print(t[0])
     # # print(sum([(len(get_id_nodes_book(path, i))) for i in range(len(t))])/len(t))
     # d =get_id_nodes_book(path, 36)
@@ -72,23 +116,23 @@ if __name__ == '__main__':
     #         res.append(get_galaxie_from_id_node(path, j))
     #     livre[i] = set(res)
     # print(livre)
-
-    X = np.matrix([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0]])
-    X = np.matrix([[0, 1, 0],
-                   [1, 0, 0],
-                   [0, 1, 0],
-                   [1, 0, 1],
-                   [0, 1, 0],
-                   [1,0,0],
-                   [0,2,0],
-                   [2,0,1],
-                   [0,1,0],
-                   [0,1,0]])
-
-    Y = np.array(([1,2,1,2,3,2,1,2,1,3]))
-    tsne = TSNE(n_components=2, verbose=1)
-    res = tsne.fit_transform(X)
-    print(res)
-    plt.scatter([res[i]], Y)
-    plt.show()
-    plt.close()
+    #
+    # X = np.matrix([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0]])
+    # X = np.matrix([[0, 1, 0],
+    #                [1, 0, 0],
+    #                [0, 1, 0],
+    #                [1, 0, 1],
+    #                [0, 1, 0],
+    #                [1,0,0],
+    #                [0,2,0],
+    #                [2,0,1],
+    #                [0,1,0],
+    #                [0,1,0]])
+    #
+    # Y = np.array(([1,2,1,2,3,2,1,2,1,3]))
+    # tsne = TSNE(n_components=2, verbose=1)
+    # res = tsne.fit_transform(X)
+    # print(res)
+    # plt.scatter([res[i]], Y)
+    # plt.show()
+    # plt.close()
