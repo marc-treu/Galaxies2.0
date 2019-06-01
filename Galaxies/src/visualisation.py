692c19b906c80822f290959424b9667bf75cb75b
ismat <auber-issam@hotmail.fr>
2019-06-02 01:43:50 +0200

    ajout d'une execution d'un k-means

--- Galaxies/src/visualisation.py
+++ Galaxies/src/visualisation.py
@@ -5,6 +5,9 @@ import shelve
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from yellowbrick.text import TSNEVisualizer
+import sklearn.manifold as ts
+from kmeans import Kmeans
+from sklearn.decomposition import TruncatedSVD, PCA
 
 
 def get_books(project_path):
@@ -60,10 +63,6 @@ def inverse_bd(project_path, list_galaxie):
 if __name__ == '__main__':
     path = "../projects/projet2"
 
-    # liste_galaxie = get_list_galaxie(path)
-    # print([liste_galaxie])
-    # inverse_bd(path,liste_galaxie)
-
     t = get_books(path)
     result = []
     for i in range(1, len(t) + 1):
@@ -77,11 +76,6 @@ if __name__ == '__main__':
     liste_galaxies = get_list_galaxie(path)
 
     matrix = np.zeros([len(t), len(liste_galaxies)])
-    """
-    print("index =", index)
-    print("matrix =", matrix)
-    print("result =", result)
-    print("liste_galaxies =", liste_galaxies)"""
 
     dirGalaxies = shelve.open(path + '/BDs/listeGalaxies')
 
@@ -92,49 +86,25 @@ if __name__ == '__main__':
         matrix[:,galaxie] = matrix[:,galaxie] / len(dirGalaxies[str(liste_galaxies[galaxie])])
 
     dirGalaxies.close()
-    print("matrix =", matrix[:10])
-
+    
     label = np.array([i for i in range(len(t))])
-
+    
     tsne = TSNEVisualizer(decompose='svd',decompose_by=15)
     tsne.fit(matrix, label)
+    print(tsne.transformer_)
     tsne.poof()
 
-
-
-
-
-    # print(t[0])
-    # # print(sum([(len(get_id_nodes_book(path, i))) for i in range(len(t))])/len(t))
-    # d =get_id_nodes_book(path, 36)
-    # print(d)
-    # print(t)
-    # livre = dict()
-    # for i in range(len(t)):
-    #     print(i)
-    #     res = []
-    #     for j in get_id_nodes_book(path, i):
-    #         # print(j)
-    #         res.append(get_galaxie_from_id_node(path, j))
-    #     livre[i] = set(res)
-    # print(livre)
-    #
-    # X = np.matrix([[5, 0, 0], [4, 0, 0], [3, 0, 0], [2, 0, 0]])
-    # X = np.matrix([[0, 1, 0],
-    #                [1, 0, 0],
-    #                [0, 1, 0],
-    #                [1, 0, 1],
-    #                [0, 1, 0],
-    #                [1,0,0],
-    #                [0,2,0],
-    #                [2,0,1],
-    #                [0,1,0],
-    #                [0,1,0]])
-    #
-    # Y = np.array(([1,2,1,2,3,2,1,2,1,3]))
-    # tsne = TSNE(n_components=2, verbose=1)
-    # res = tsne.fit_transform(X)
-    # print(res)
-    # plt.scatter([res[i]], Y)
-    # plt.show()
-    # plt.close()
+    svd = TruncatedSVD(n_components=15)
+    svd_matrix = svd.fit_transform(matrix)
+    tsne = ts.TSNE()
+    y = tsne.fit_transform(svd_matrix)
+    kmeans = Kmeans(5,200,0.1)
+    kmeans.fit(y)
+    for i in range(kmeans.nb_cluster):
+        print("Cluster ",i)
+        print((np.where(kmeans.which_cluster == i))[0])
+        print()
+    plt.scatter(y[:, 0], y[:, 1], c=kmeans.which_cluster.reshape(-1,1), s=50, cmap='viridis')
+    plt.title("Resultat du clustering")
+    plt.savefig("clustering")
+    plt.show()
